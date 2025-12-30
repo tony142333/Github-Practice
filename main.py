04b3f1d2336b1f70c3ee23595f477ebbe0b233bb
Tarun sai teja Vaskuri <tarunsaitejavaskuri@gmail.com>
2025-12-30 12:33:13 +0530

    adding to main branch

---
+++ main.py
@@ -0,0 +1,11 @@
+def adding(a:int,b:int):
+    return a+b
+def subtracting(a:int,b:int):
+    return abs(a-b) 
+def multiply(a,b):
+    return (a*b)
+
+
+print(adding(5,3))
+print(subtracting(2,3))
+##hello#hi  
